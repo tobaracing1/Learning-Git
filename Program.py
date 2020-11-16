43b003881d5f35a1ac2ea8618cf8a5d7f7f52ba8
tobaracing1 <tobaracing1@gmail.com>
2020-11-16 10:17:23 +0700

    Penambahan penjelasan ke Program.py

--- Program.py
+++ Program.py
@@ -1,10 +1,14 @@
 #Program mengubah data list menjadi data unique
+
+#Program untuk mengubah data
 def set_change(uniq):
     him = set()
     for i in uniq:
         him.add(i)
     return him
 
+#Contoh list yang akan diubah    
 list1 = [1,2,3,3,3,3,4,5]
 
+#Perintah Output ke layar User
 print (set_change(list1))
